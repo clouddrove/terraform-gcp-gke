469ce5aa890ad0c139ecc0e2ff2fe6c0e8487491
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2024-08-27 00:46:57 +0530

    feat: resolved tfcheck issue

--- main.tf
+++ main.tf
@@ -64,9 +64,9 @@ resource "google_container_cluster" "primary" {
 
         labels = var.labels
 
-      workload_metadata_config {
-        mode = var.workload_metadata_mode
-    }
+        workload_metadata_config {
+          mode = var.workload_metadata_mode
+        }
       }
     }
   }
