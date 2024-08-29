39b69facc22b5102e566e3d0788e7335328772e6
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2024-08-29 15:57:51 +0530

    feat: tfcheck issue

--- examples/complete-example/versions.tf
+++ examples/complete-example/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">=1.7.0"
+  required_version = ">=1.3"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.22.0"
+      version = ">= 5.9.0, < 6"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.22.0"
+      version = ">= 5.9.0, < 6"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
