541ad76728dacfce6495a2e9bed1acf781ada10b
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2025-07-28 14:40:44 +0530

    feat: added old version

--- examples/complete-example/versions.tf
+++ examples/complete-example/versions.tf
@@ -4,13 +4,14 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 6.45.0"
+      #version = ">= 6.45.0"
+      version = ">= 5.9.0, < 7"
     }
-    # google-beta = {
-    #   source  = "hashicorp/google-beta"
-    #   #version = ">= 6.45.0"
-    #   version = ">= 5.9.0, < 7"
-    # }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      #version = ">= 6.45.0"
+      version = ">= 5.9.0, < 7"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.10"
