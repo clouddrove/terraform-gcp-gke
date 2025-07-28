07b5a0c41eb73f5a823a3014d8b96ce5dd60f4e5
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2025-07-28 22:29:54 +0530

    feat: fixing lint issue

--- cluster.tf
+++ cluster.tf
@@ -370,10 +370,6 @@ resource "google_container_cluster" "primary" {
       image_type       = lookup(var.node_pools[0], "image_type", "COS_CONTAINERD")
       machine_type     = lookup(var.node_pools[0], "machine_type", "e2-medium")
       min_cpu_platform = lookup(var.node_pools[0], "min_cpu_platform", "")
-
-      metadata = {
-        disable-legacy-endpoints = "true"
-      }
       dynamic "gcfs_config" {
         for_each = lookup(var.node_pools[0], "enable_gcfs", false) ? [true] : []
         content {
@@ -399,21 +395,21 @@ resource "google_container_cluster" "primary" {
 
       logging_variant = lookup(var.node_pools[0], "logging_variant", "DEFAULT")
 
-      # dynamic "workload_metadata_config" {
-      #   for_each = local.cluster_node_metadata_config
+      dynamic "workload_metadata_config" {
+        for_each = local.cluster_node_metadata_config
 
-      #   content {
-      #     mode = lookup(each.value, "node_metadata", workload_metadata_config.value.mode)
-      #   }
-      # }
-
-      workload_metadata_config {
-        mode = "GKE_METADATA_SERVER" # or "SECURE"
+        content {
+          mode = lookup(each.value, "node_metadata", workload_metadata_config.value.mode)
+        }
       }
 
+      # workload_metadata_config {
+      #   mode = "GKE_METADATA"
+      # }
 
 
-      # metadata = local.node_pools_metadata["all"]
+
+      metadata = local.node_pools_metadata["all"]
 
       dynamic "sandbox_config" {
         for_each = tobool((lookup(var.node_pools[0], "sandbox_enabled", var.sandbox_enabled))) ? ["gvisor"] : []
@@ -607,7 +603,7 @@ resource "google_container_node_pool" "pools" {
     min_cpu_platform = lookup(each.value, "min_cpu_platform", "")
 
     metadata = {
-      disable-legacy-endpoints = "true"
+      disable-legacy-endpoints = true
     }
     dynamic "gcfs_config" {
       for_each = lookup(each.value, "enable_gcfs", false) ? [true] : []
